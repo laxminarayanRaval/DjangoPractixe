d11c811335908dc2a3b8df6bf95f895b122f43ac
Laxminarayan Raval <ln.raval01@gmail.com>
2022-03-14 12:20:03 +0530

    Admin Configuration Custom

--- projectlx/polls/admin.py
+++ projectlx/polls/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['que_text']}),
+        ('Date Information', {'fields': ['pub_date']})
+    ]
+    # fields = ['pub_date', 'que_text']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
